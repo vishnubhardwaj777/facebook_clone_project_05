b7b593c1e9294ca9dc5948e7e1baa634ebb45ff8
main <vishnubhardwaj352@gmail.com>
2023-02-25 12:19:39 +0530

    first_commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish: ["Mulish", "sans-serif"],
+        vishnu:["SFProDisplay-Regular", "Helvetica"],
+      },
+    },
+  },
+  plugins: [],
+}
